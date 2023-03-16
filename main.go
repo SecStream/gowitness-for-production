5918aa4c9c61d7491029e3524d1c4bab72be7afb
39ff <open@cse.jp>
2023-03-16 17:16:49 +0900

    delete sentry

--- main.go
+++ main.go
@@ -2,27 +2,13 @@ package main
 
 import (
 	"embed"
-	"github.com/getsentry/sentry-go"
 	"github.com/sensepost/gowitness/cmd"
-	"log"
-	"os"
 )
 
 //go:embed web/assets/* web/ui-templates/* web/static-templates/*
 var assets embed.FS
 
 func main() {
-
-	err := sentry.Init(sentry.ClientOptions{
-		Dsn: os.Getenv("SENTRY_DSN"),
-		// Set TracesSampleRate to 1.0 to capture 100%
-		// of transactions for performance monitoring.
-		// We recommend adjusting this value in production,
-		TracesSampleRate: 0,
-	})
-	if err != nil {
-		log.Fatalf("sentry.Init: %s", err)
-	}
 	cmd.Embedded = assets
 	cmd.Execute()
 }
